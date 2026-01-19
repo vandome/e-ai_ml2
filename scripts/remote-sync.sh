12123e44988aab731a6c860a7c3939c58762c15f
Damien Decremer <damien.decremer@ecmwf.int>
2026-01-19 16:05:22 +0000

    same

--- scripts/remote-sync.sh
+++ scripts/remote-sync.sh
@@ -6,7 +6,7 @@ PULL_REMOTE="${PULL_REMOTE:-origin}"
 BRANCH="${BRANCH:-main}"
 
 git config --global remote.pushDefault "$PUSH_REMOTE"
-git branch --set-upstream-to="$PULL_REMOTE/$BRANCH" "$BRANCH"
+git branch --set-upstream-to="$PUSH_REMOTE/$BRANCH" "$BRANCH"
 git fetch "$PULL_REMOTE"
 git checkout "$BRANCH"
 git merge "$PULL_REMOTE/$BRANCH"
